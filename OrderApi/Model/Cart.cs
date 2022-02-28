0de2a5df121ddfd43aa03a586977d5e17303e294
alvinsoo <34090577+alvinsoo@users.noreply.github.com>
2022-02-28 15:10:23 +0800

    Add files via upload

---
+++ OrderApi/Model/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public double ProductPrice { get; set; }
+        public double Total { get; set; }
+        public string OrderStatus { get; set; }
+        public int OrderId { get; set; }
+    }
+}
---
+++ OrderApi/Model/CartsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Models
+{
+    public class CartsContext : DbContext
+    {
+        public CartsContext(DbContextOptions<CartsContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Cart> Carts { get; set; } = null!;
+    }
+}
